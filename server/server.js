18d8c7cc01514d94bfda3345057739d729deaf39
mshivansh616-svg <mshivansh616@gmail.com>
2026-09-24 20:29:19 +0530

    Update server configuration

--- server/server.js
+++ server/server.js
@@ -103,8 +103,8 @@ app.delete("/api/complaints/:id", async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
